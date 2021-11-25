bcf6ff7f5f16ac64281f6bc96822896500e1f420
lalilu <lalilu>
2021-11-25 12:10:52 +0800

    创建TextAlphaHelper类，用于保存文字的透明度

---
+++ multiline-collapsingtoolbar/src/main/java/net/opacapp/multilinecollapsingtoolbar/TextAlphaHelper.java
@@ -0,0 +1,26 @@
+package net.opacapp.multilinecollapsingtoolbar;
+
+import android.text.TextPaint;
+
+public class TextAlphaHelper {
+    /**
+     * 实现自定义透明度
+     */
+    private boolean textAlpha = true;
+    private int textAlphaValue = 255;
+
+    public void updateTextAlpha(int value) {
+        this.textAlpha = value >= 0 && value <= 255;
+        this.textAlphaValue = value;
+    }
+
+    public boolean shouldDrawAlpha() {
+        return textAlpha || textAlphaValue == 0;
+    }
+
+    public void autoSetAlphaToTextPaint(TextPaint toTextPaint) {
+        if (shouldDrawAlpha()) {
+            toTextPaint.setAlpha(textAlphaValue);
+        }
+    }
+}
